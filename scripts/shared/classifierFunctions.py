bebd3a3c8f48ed0495b354e1c81fa4eeb646b599
RushilDaya <rushildayabme@gmail.com>
2019-05-30 13:23:32 +0200

    completed classification pipeline script

--- scripts/shared/classifierFunctions.py
+++ scripts/shared/classifierFunctions.py
@@ -1,7 +1,13 @@
 # the top level functions for classification
 from shared.autoencoderHelpers import read_n_images, generate_img_from_folder, get_input_shape, get_num_examples, plot_history, get_images, bgr2rgb, plot_reconstruction
-from shared.classifierHelpers import getUntrained, getFrozen, getMelted, get_num_classes, autoencoderToClassifier
+from shared.classifierHelpers import getUntrained, getFrozen, getMelted, get_num_classes, autoencoderToClassifier, generate_img_label_from_folder
 from shared.autoencoderArchitectures import getCodeLabel
+
+import numpy as np
+from math import ceil
+from random import randint
+import pickle, os
+import tensorflow as tf
 
 def buildModels(aeArch, aeModel, clfModel, optimizer, datapath='', modelpath=''):
 
@@ -25,11 +31,60 @@ def buildModels(aeArch, aeModel, clfModel, optimizer, datapath='', modelpath='')
 
 def trainModels(frozenModel,meltedModel,untrainedModel, batchSize, epochs, datapath=''):
 
-    frozenModel_OBJ = None
-    meltedModel_OBJ = None
-    untrainedModel_OBJ = None
+    NUM_SAMPLES_TRAIN = get_num_examples(datapath, 'training')
+    STEPS_PER_EPOCH = ceil(NUM_SAMPLES_TRAIN/batchSize)
+
+    NUM_SAMPLES_VAL = get_num_examples(datapath, 'validation')
+    VALIDATION_STEPS=ceil(NUM_SAMPLES_VAL/batchSize)
+
+    early_stop = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=10)
+    historyFrozen = frozenModel.fit_generator(generate_img_label_from_folder(datapath, 'training', batchSize), shuffle=True,
+                                    validation_data=generate_img_label_from_folder(datapath, 'validation', batchSize),
+                                    steps_per_epoch=STEPS_PER_EPOCH, validation_steps=VALIDATION_STEPS,
+                                    epochs=epochs)
+    frozenModel_OBJ = (frozenModel, historyFrozen)
+
+    early_stop = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=10)
+    historyMelted = meltedModel.fit_generator(generate_img_label_from_folder(datapath, 'training', batchSize), shuffle=True,
+                                    validation_data=generate_img_label_from_folder(datapath, 'validation', batchSize),
+                                    steps_per_epoch=STEPS_PER_EPOCH, validation_steps=VALIDATION_STEPS,
+                                    epochs=epochs)
+    meltedModel_OBJ = (meltedModel, historyMelted)
+
+    early_stop = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=10)
+    historyUntrained = untrainedModel.fit_generator(generate_img_label_from_folder(datapath, 'training', batchSize), shuffle=True,
+                                    validation_data=generate_img_label_from_folder(datapath, 'validation', batchSize),
+                                    steps_per_epoch=STEPS_PER_EPOCH, validation_steps=VALIDATION_STEPS,
+                                    epochs=epochs)
+    untrainedModel_OBJ = (untrainedModel, historyUntrained)
+
     return frozenModel_OBJ, meltedModel_OBJ, untrainedModel_OBJ
 
 def saveModels(configName, frozenModel_OBJ, meltedModel_OBJ, untrainedModel_OBJ, savepath=''):
+
+    baseDir = savepath + '/'+configName +'/'
+    saveLocFrozen = baseDir + 'frozen/'
+    saveLocMelted = baseDir + 'melted/'
+    saveLocUntrained = baseDir + 'untrained/'
+
+    if not os.path.isdir(baseDir):
+        os.mkdir(baseDir)
+    if not os.path.isdir(saveLocFrozen):
+        os.mkdir(saveLocFrozen)
+    if not os.path.isdir(saveLocMelted):
+        os.mkdir(saveLocMelted)
+    if not os.path.isdir(saveLocUntrained):
+        os.mkdir(saveLocUntrained)   
+
+    tf.keras.models.save_model(frozenModel_OBJ[0], saveLocFrozen+'model', overwrite=True)
+    tf.keras.models.save_model(meltedModel_OBJ[0], saveLocMelted+'model', overwrite=True)
+    tf.keras.models.save_model(untrainedModel_OBJ[0], saveLocUntrained+'model', overwrite=True)         
+
+    with open(saveLocFrozen+'history.pickle', 'wb' ) as f:
+        pickle.dump(frozenModel_OBJ[1].history, f)
+    with open(saveLocMelted+'history.pickle', 'wb' ) as f:
+        pickle.dump(meltedModel_OBJ[1].history, f)
+    with open(saveLocUntrained+'history.pickle', 'wb' ) as f:
+        pickle.dump(untrainedModel_OBJ[1].history, f)
 
     return True
--- scripts/shared/classifierHelpers.py
+++ scripts/shared/classifierHelpers.py
@@ -3,8 +3,10 @@ import tensorflow as tf
 import pickle
 import glob
 import cv2
+import numpy as np
 from tensorflow.keras.models import Model, Sequential
 from tensorflow.keras import backend as K
+from shared.autoencoderHelpers import read_n_images
 from shared.autoencoderArchitectures import getNormal, getDayaNet, getLeCunhaNet 
 from shared.classifierArchitectures import getSimple
 
@@ -58,7 +60,7 @@ def get_num_classes(data_dir):
     return len(data[modes[i]][i][-1])
 
 def autoencoderToClassifier(autoencoderModel, classifierName, codeName, outputShape):
-    
+
     layerNames = [layer.name for layer in autoencoderModel.layers]
     encodedLayerId = layerNames.index(codeName)
     encoder = Model(inputs=autoencoderModel.inputs, outputs=autoencoderModel.layers[encodedLayerId].output)
@@ -70,3 +72,24 @@ def autoencoderToClassifier(autoencoderModel, classifierName, codeName, outputSh
 
     classifier = Model(inputs=encoder.inputs, outputs=classifierOutput)
     return classifier
+
+def generate_img_label_from_folder(data_dir, mode, batch_size):
+    """
+
+    """
+    loc = "{}/{}".format(data_dir, mode)
+    while True:
+        with open('{}/labels.pickle'.format(data_dir), 'rb') as f:
+            data = pickle.load(f)
+
+        modes = list(data.keys())
+        del modes[-1]
+
+        assert mode in modes, "'{}' not a valid mode (must be one of {})".format(mode, str(modes))
+        assert glob.glob(loc), "Check directory."
+        assert glob.glob("{}/*.jpg".format(loc)), "Check file extension (should be 'jpg')."
+        for idx in range(0, len(data[mode]), batch_size):
+            start = idx
+            end = idx + batch_size
+            images, labels = read_n_images(data[mode], start, end, loc)
+            yield (images/255, np.array(labels))
