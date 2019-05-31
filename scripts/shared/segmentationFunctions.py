85bddc6823a5ea96019ec024e6ab00fd3cb70069
RushilDaya <rushildayabme@gmail.com>
2019-05-31 12:33:58 +0200

    added prototype for segmentation code as well as top level code

---
+++ scripts/shared/segmentationFunctions.py
@@ -0,0 +1,14 @@
+import numpy as np
+from math import ceil
+from random import randint
+import pickle, os
+import tensorflow as tf
+
+def builModel(segmentationArchitecture, optimizer,datapath=''):
+    return None
+
+def trainModel(model, batchSize, datapath=''):
+    return None
+
+def saveModel(configName, model, savepath=''):
+    return None
