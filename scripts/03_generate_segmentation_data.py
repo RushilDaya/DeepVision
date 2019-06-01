377fadda0d6633e065fa0a89d026bdc85b510317
RushilDaya <rushildayabme@gmail.com>
2019-06-01 19:49:08 +0200

    added image segment generator for persons

--- scripts/03_generate_segmentation_data.py
+++ scripts/03_generate_segmentation_data.py
@@ -45,8 +45,38 @@ def getProcesser(segmentationType):
             return image
         return binaryForegroundBackground
 
+    if segmentationType == 'personOnly':
+        def binaryPersonOnly(image):
+            # figuring out the segmentation value requires a bit of trail and error
+            (height,width,depth) = image.shape
+            for i in range(height):
+                for j in range(width):
+                    if (image[i,j,0] > 177 and image[i,j,0] < 182) and (image[i,j,1] > 125 and image[i,j,1] < 130) and  (image[i,j,2] > 230 and image[i,j,2] < 235):
+                        image[i,j,:] = [255,255,255]
+                    else:
+                        image[i,j,:] = [0,0,0]
+            return image
+        return binaryPersonOnly
+
+
     else:
         raise TypeError('segmentation type not defined')
+
+
+def singleOutClass(allNames, classOfInterest, rawDataPath):
+    with open(rawDataPath+'ImageSets/Main/{}_trainval.txt'.format(classOfInterest)) as file:
+        dirtyLines = file.read().splitlines()
+
+    classImageNames = []
+    for i in range(len(dirtyLines)):
+        if dirtyLines[i][-2:]==' 1':
+            classImageNames.append(dirtyLines[i][:-3])
+
+    newList = list(set(allNames).intersection(classImageNames))
+    print(len(newList))
+    print('---')
+    return newList
+
 
 
 rawDataPath = confReader('RAW_DATA_LOCATION')
@@ -56,7 +86,7 @@ if not os.path.isdir(rawDataPath):
 
 imageSize=confReader('IMAGE_SIZE')
 segmentationType = confReader('SEGMENTATION_DATA_TYPE')
-if not segmentationType in ['foregroundBackground']:
+if not segmentationType in ['foregroundBackground','personOnly']:
     print('invalid segmentation type in configFile')
     sys.exit()
 
@@ -79,6 +109,9 @@ with open(rawDataPath+'ImageSets/Segmentation/trainval.txt') as file:
     allImageNames = file.read().splitlines()
 
 
+if segmentationType == 'personOnly':
+    allImageNames = singleOutClass(allImageNames,'person', rawDataPath)
+
 numImages = len(allImageNames)
 trainingNames = allImageNames[:int(numImages*trainingSplit)]
 validationNames = allImageNames[int(numImages*trainingSplit):int(numImages*(trainingSplit+validationSplit))]
@@ -97,21 +130,21 @@ with open('../data/segmentation/'+segmentationType+'/labels.pickle', 'wb' ) as f
 
 
 trainingImagePaths = [rawDataPath+'JPEGImages/'+item+'.jpg' for item in trainingNames]
-trainingLabelPaths = [rawDataPath+'SegmentationObject/'+item+'.png' for item in trainingNames]
+trainingLabelPaths = [rawDataPath+'SegmentationClass/'+item+'.png' for item in trainingNames]
 trainingImagePathsTarget = [storeDir+'training/images/'+item+'.jpg' for item in trainingNames]
 trainingLabelPathsTarget = [storeDir+'training/labels/'+item+'.jpg' for item in trainingNames]
 copyAndResizeImage(trainingImagePaths,trainingImagePathsTarget,imageSize,processFunction=None)
 copyAndResizeImage(trainingLabelPaths,trainingLabelPathsTarget,imageSize,processFunction=getProcesser(segmentationType))
 
 validationImagePaths = [rawDataPath+'JPEGImages/'+item+'.jpg' for item in validationNames]
-validationLabelPaths = [rawDataPath+'SegmentationObject/'+item+'.png' for item in validationNames]
+validationLabelPaths = [rawDataPath+'SegmentationClass/'+item+'.png' for item in validationNames]
 validationImagePathsTarget = [storeDir+'validation/images/'+item+'.jpg' for item in validationNames]
 validationLabelPathsTarget = [storeDir+'validation/labels/'+item+'.jpg' for item in validationNames]
 copyAndResizeImage(validationImagePaths,validationImagePathsTarget,imageSize,processFunction=None)
 copyAndResizeImage(validationLabelPaths,validationLabelPathsTarget,imageSize,processFunction=getProcesser(segmentationType))
 
 testImagePaths = [rawDataPath+'JPEGImages/'+item+'.jpg' for item in testNames]
-testLabelPaths = [rawDataPath+'SegmentationObject/'+item+'.png' for item in testNames]
+testLabelPaths = [rawDataPath+'SegmentationClass/'+item+'.png' for item in testNames]
 testImagePathsTarget = [storeDir+'test/images/'+item+'.jpg' for item in testNames]
 testLabelPathsTarget = [storeDir+'test/labels/'+item+'.jpg' for item in testNames]
 copyAndResizeImage(testImagePaths,testImagePathsTarget,imageSize,processFunction=None)
